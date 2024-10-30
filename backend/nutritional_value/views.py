8748418d945433fe10d080aabdef00e5343b5000
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-10-31 01:52:26 +0300

    Add base and index page templates

--- backend/nutritional_value/views.py
+++ backend/nutritional_value/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Ingredient
+
+
+def index(request):
+    """Функция отображения главной страницы"""
+    context = {
+        'num_ingredients': Ingredient.objects.all().count()
+    }
+    return render(request, 'index.html', context,)
