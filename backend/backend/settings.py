7e344254487e0d94bd6e32bcc9fa13902e609302
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-10-30 15:38:03 +0300

    Fixed app name in installed apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.NutritionalValueConfig',
+    'nutritional_value.apps.NutritionalValueConfig',
 ]
 
 MIDDLEWARE = [
