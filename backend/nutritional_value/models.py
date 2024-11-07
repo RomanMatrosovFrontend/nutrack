6b451cfac7a5a8e0b7cb45626abae96af5ffa92f
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-11-07 09:19:27 +0300

    Make ingredient title unique

--- backend/nutritional_value/models.py
+++ backend/nutritional_value/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True, null=True)
 
