efa2588521efeae02e180530168f201a22861703
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-11-07 09:15:13 +0300

    Add edit and delete functions for ingredients

--- backend/nutritional_value/urls.py
+++ backend/nutritional_value/urls.py
@@ -7,8 +7,17 @@ urlpatterns = [
     path('add/', views.add_ingredient, name='add_ingredient'),
     path('ingredients/', views.ingredient_list, name='ingredient_list'),
     path(
-        'ingredients/<str:ingredient_slug>',
+        'ingredients/<slug:ingredient_slug>/',
         views.ingredient_detail,
         name='ingredient_detail'
     ),
+    path(
+        'ingredients/<slug:ingredient_slug>/edit/',
+        views.ingredient_edit, name='ingredient_edit'
+    ),
+    path(
+        'ingredients/<slug:ingredient_slug>/delete/',
+        views.ingredient_delete, name='ingredient_delete'
+    ),
+
 ]
--- backend/nutritional_value/views.py
+++ backend/nutritional_value/views.py
@@ -34,3 +34,34 @@ def ingredient_list(request):
 def ingredient_detail(request, ingredient_slug):
     ingredient = get_object_or_404(Ingredient, slug=ingredient_slug)
     return render(request, 'ingredient_detail.html', {'ingredient': ingredient})
+
+
+def ingredient_edit(request, ingredient_slug):
+    ingredient = get_object_or_404(Ingredient, slug=ingredient_slug)
+
+    if request.method == 'POST':
+        form = IngredientForm(request.POST, instance=ingredient)
+        if form.is_valid():
+            form.save()
+            return redirect(
+                'ingredient_detail', ingredient_slug=ingredient.slug
+            )
+    else:
+        form = IngredientForm(instance=ingredient)
+
+    return render(
+        request, 'ingredient_edit.html',
+        {'form': form, 'ingredient': ingredient}
+    )
+
+
+def ingredient_delete(request, ingredient_slug):
+    ingredient = get_object_or_404(Ingredient, slug=ingredient_slug)
+
+    if request.method == 'POST':
+        ingredient.delete()
+        return redirect('ingredient_list')
+
+    return render(
+        request, 'ingredient_confirm_delete.html', {'ingredient': ingredient}
+    )
