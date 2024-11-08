47509f12660698322a15f9d01ca7e2250f372011
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-11-08 23:48:11 +0300

    Add logout button

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -5,4 +6,9 @@ from . import views
 
 urlpatterns = [
     path('registration/', views.registration, name='registration'),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(next_page='index'),
+        name='logout'
+    ),
 ]
