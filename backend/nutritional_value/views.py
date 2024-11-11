40658669c5032e7b1c066e81112d63e58f68cd65
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-11-11 03:04:28 +0300

    Add pagination for ingredients list

--- backend/nutritional_value/views.py
+++ backend/nutritional_value/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from core.views import custom_forbidden_view
@@ -39,9 +40,12 @@ def add_ingredient(request):
 
 def ingredient_list(request):
     ingredients = Ingredient.objects.all()
+    paginator = Paginator(ingredients, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     return render(
         request, 'nutritional_value/ingredient_list.html',
-        {'ingredients': ingredients}
+        {'page_obj': page_obj}
     )
 
 
