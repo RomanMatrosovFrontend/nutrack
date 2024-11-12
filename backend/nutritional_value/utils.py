b583cf4393ad9cccbc2c97e48e5b1cb57a9d7775
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-11-12 04:38:40 +0300

    Optimize get_total_nutrients function

--- backend/nutritional_value/utils.py
+++ backend/nutritional_value/utils.py
@@ -1,58 +1,26 @@
-from django.db.models import Sum, F
+from django.db.models import Count, F, Sum
 
 
 def get_total_nutrients(objects):
-    total_count = objects.count()
+    total_data = objects.aggregate(
+        total_calories=Sum(F('ingredient__calorie_content') * F('grams')),
+        total_proteins=Sum(F('ingredient__proteins') * F('grams')),
+        total_fats=Sum(F('ingredient__fats') * F('grams')),
+        total_carbohydrates=Sum(F('ingredient__carbohydrates') * F('grams')),
+        total_count=Count('id')
+    )
 
-    if total_count == 0:
-        return {
-            'total_calories_value': 0,
-            'total_proteins_value': 0,
-            'total_fats_value': 0,
-            'total_carbohydrates_value': 0,
-            'average_calories_value': 0,
-            'average_proteins_value': 0,
-            'average_fats_value': 0,
-            'average_carbohydrates_value': 0,
-            'total_count': 0
-        }
-
-    total_calories = objects.annotate(
-        calories=F('ingredient__calorie_content') * F('grams')
-    ).aggregate(total_calories=Sum('calories'))
-
-    total_calories_value = total_calories['total_calories'] or 0
-    average_calories_value = total_calories_value / total_count
-
-    total_proteins = objects.annotate(
-        proteins=F('ingredient__proteins') * F('grams')
-    ).aggregate(total_proteins=Sum('proteins'))
-
-    total_proteins_value = total_proteins['total_proteins'] or 0
-    average_proteins_value = total_proteins_value / total_count
-
-    total_fats = objects.annotate(
-        fats=F('ingredient__fats') * F('grams')
-    ).aggregate(total_fats=Sum('fats'))
-
-    total_fats_value = total_fats['total_fats'] or 0
-    average_fats_value = total_fats_value / total_count
-
-    total_carbohydrates = objects.annotate(
-        carbohydrates=F('ingredient__carbohydrates') * F('grams')
-    ).aggregate(total_carbohydrates=Sum('carbohydrates'))
-
-    total_carbohydrates_value = total_carbohydrates['total_carbohydrates'] or 0
-    average_carbohydrates_value = total_carbohydrates_value / total_count
-
-    return {
-        'total_calories_value': total_calories_value,
-        'total_proteins_value': total_proteins_value,
-        'total_fats_value': total_fats_value,
-        'total_carbohydrates_value': total_carbohydrates_value,
-        'average_calories_value': average_calories_value,
-        'average_proteins_value': average_proteins_value,
-        'average_fats_value': average_fats_value,
-        'average_carbohydrates_value': average_carbohydrates_value,
-        'total_count': total_count
+    total_count = total_data['total_count'] or 0
+    totals = {
+        key: total_data[key] or 0 for key in [
+            'total_calories', 'total_proteins',
+            'total_fats', 'total_carbohydrates'
+        ]
     }
+
+    averages = {}
+    for key, value in totals.items():
+        average_key = f'average_{key.split("_")[1]}'
+        averages[average_key] = value / total_count if total_count else 0
+
+    return {**totals, **averages, 'total_count': total_count}
