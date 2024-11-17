9610cc229346926a0cf459a4a51d358133a0e107
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-11-18 00:40:40 +0300

    Add validation for numeric fields in the Ingredient model (calories, proteins, etc.) to ensure they're non-negative.

--- backend/nutritional_value/models.py
+++ backend/nutritional_value/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 User = get_user_model()
@@ -10,10 +11,10 @@ class Ingredient(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True, null=True)
 
-    calorie_content = models.FloatField()
-    proteins = models.FloatField()
-    fats = models.FloatField()
-    carbohydrates = models.FloatField()
+    calorie_content = models.FloatField(validators=[MinValueValidator(0.0)])
+    proteins = models.FloatField(validators=[MinValueValidator(0.0)])
+    fats = models.FloatField(validators=[MinValueValidator(0.0)])
+    carbohydrates = models.FloatField(validators=[MinValueValidator(0.0)])
 
     has_animal_proteins = models.BooleanField()
 
@@ -26,7 +27,7 @@ class Ingredient(models.Model):
 class AmountPerDay(models.Model):
     date = models.DateField()
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    grams = models.FloatField()
+    grams = models.FloatField(validators=[MinValueValidator(0.0)])
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
