1a03bf03532b569c862b61eff4a00e8fb761c414
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-11-18 09:25:16 +0300

    Edit user app forms styles

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -5,14 +5,51 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={'class': 'form-input'})
+    )
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-input'}),
+        help_text='''
+            Ваш пароль должен содержать как минимум 8 символов.
+            Ваш пароль не может состоять только из цифр.
+        '''
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={'class': 'form-input'}),
+        help_text='Для подтверждения введите, пожалуйста, пароль ещё раз.'
+    )
+    first_name = forms.CharField(
+        label='Имя',
+        widget=forms.TextInput(attrs={'class': 'form-input'})
+    )
+    last_name = forms.CharField(
+        label='Фамилия',
+        widget=forms.TextInput(attrs={'class': 'form-input'})
+    )
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-input'})
+    )
+
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'username', 'email')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
 
 
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.CharField(label='Имя пользователя', max_length=150)
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    username = forms.CharField(
+        label='Имя пользователя',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
 
 
 class ProfileEditForm(forms.ModelForm):
@@ -33,12 +70,17 @@ class ProfileEditForm(forms.ModelForm):
             'last_name': 'Фамилия',
             'email': 'Email',
         }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
 
 
 class PasswordEditForm(forms.ModelForm):
     password1 = forms.CharField(
         label='Новый пароль',
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
         help_text='''
             Ваш пароль должен содержать как минимум 8 символов.
             Ваш пароль не может состоять только из цифр.
@@ -46,7 +88,7 @@ class PasswordEditForm(forms.ModelForm):
     )
     password2 = forms.CharField(
         label='Подтверждение пароля',
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
         help_text='Для подтверждения введите, пожалуйста, пароль ещё раз.'
     )
 
