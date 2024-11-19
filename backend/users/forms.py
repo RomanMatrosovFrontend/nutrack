1a9acca5d13cef335b217591f7f1a25a8be5153a
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2024-11-19 12:28:55 +0300

    Edit styles of tables and forms

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -37,7 +37,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+        fields = (
+            'first_name', 'last_name', 'username',
+            'email', 'password1', 'password2'
+        )
 
 
 class CustomAuthenticationForm(AuthenticationForm):
